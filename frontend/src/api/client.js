20f814fc1c00a8b9b1b1da99a222dd3706ba406d
EMMANUEL08161823021 <117604958+EMMANUEL08161823021@users.noreply.github.com>
2025-09-16 11:07:59 +0100

    change

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:9000/",
+  baseURL: "https://neurowallet.onrender.com",
   // "https://neurowallet.onrender.com",
   withCredentials: true,
 });
